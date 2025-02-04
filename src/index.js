8992d6e18810772fd8e8b2087f63ba60327062c2
heskinstad <heskinstad@gmail.com>
2025-02-04 10:59:39 +0100

    Finished Core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-
 console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
+const cards = document.querySelector(".cards");
+
+function firstLetterToUpper(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+function makeCard(id) {
+    // create card
+    const card = document.createElement("li");
+    card.classList.add("card");
+
+    // add title
+    const cardTitle = document.createElement("h2");
+    cardTitle.classList.add("card--title");
+    cardTitle.innerText = firstLetterToUpper(data[id].name);
+    card.append(cardTitle);
+
+    // add image
+    const cardImage = document.createElement("img");
+    cardImage.classList.add("card--img")
+    cardImage.src = data[id].sprites.other["official-artwork"].front_default;
+    card.append(cardImage);
+
+    // add stats
+    const cardStats = document.createElement("ul");
+    cardStats.classList.add("card--text");
+
+    for (let s = 0; s < 6; s++) {
+        const cardStat = document.createElement("li");
+        cardStat.innerText = data[id].stats[s].stat.name.toUpperCase() + ": " + data[id].stats[s].base_stat;
+        cardStats.append(cardStat);
+    }
+
+    card.append(cardStats);
+
+    cards.append(card);
+}
+
+for (let p = 0; p < data.length; p++) {
+    makeCard(p);
+}
+
 console.log(data[0]);
